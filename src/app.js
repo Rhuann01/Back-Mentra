128c7ed22ba90ff24399cccaff5ac0e5fb6ab6ca
Rhuann01 <rhuann2006@gmail.com>
2026-03-24 19:57:38 -0300

    fix bugs deploy v1.0

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ import iaRes from "./routes/resIA.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 app.use("/ai", iaRes);
